fad4ff6bbf44c82a8458c81f6437079c345dee3b
Viktoriya Musikhina <musihina47225@mail.ru>
2024-11-01 17:44:22 +0300

    Implement a chatserver

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Users(models.Model):
+    name = models.CharField(max_length=150)
+    avatar = models.ImageField(upload_to="users_avatar/", blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
